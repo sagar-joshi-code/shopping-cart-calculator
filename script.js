704c9729db9c335602a2547b1cbdead6d1dd327d
sagar joshi <sagarjo637@gmail.com>
2025-12-27 20:10:12 +0545

    Add total, discount, and final price calculation

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // shopping-cart-calculator
 
 let prices = [200, 450, 120, 700, 350];
+let count=0;
+let total_price=0;
+for(let i=0;i<prices.length;i++){
+    count++;
+    total_price +=prices[i];
+
+}
+console.log("Total items =",count)
+console.log("Total price =",total_price)
+let discount=total_price * 0.10;
+let final_price=total_price - discount;
+console.log("Final price after discount =",final_price)
 
